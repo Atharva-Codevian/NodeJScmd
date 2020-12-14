296c0f8dec200fc616697ffdc268bb537639b1c5
Atharva Patil <atharva.patil@codevian.com>
2020-12-14 12:03:06 +0530

    14dec commit 1

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,9 +1,5 @@
-var _ = require('underscore');
+const express = require('express');
+const app = express();
 
-//hierarchy in which the require of the node module works
 
-//core module
-//file or folder
-//node_modules
-var result =  _.contains([1,2,3],2);
-console.log(result); 
+
